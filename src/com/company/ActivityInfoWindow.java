244ee31ff7cce4870750e3e276c87b136d49d3e5
Everest#1 <kaspars.cinis@gmail.com>
2016-02-26 13:41:44 +0100

    Added new super cool stuff

--- src/com/company/ActivityInfoWindow.java
+++ src/com/company/ActivityInfoWindow.java
@@ -1,58 +1,44 @@
 package com.company;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 /**
  * Created by St_Muerte on 2/25/16.
  */
-public class ActivityInfoWindow extends Application{
-
-    TextField nameTextField = new TextField();
-    TextArea descriptionArea = new TextArea();
-    TextField ageLimitTextfield = new TextField();
-    TextField priceTextField = new TextField();
-
-
+public class ActivityInfoWindow {
+    TextField nameTextField;
+    TextArea descriptionArea;
+    TextField ageLimitTextfield;
+    TextField priceTextField;
+    Stage window;
+    MainAdminScene m;
     public String getNameTextField() {
         return nameTextField.getText().toString();
     }
-
     public void setNameTextField(String nameText) {
         nameTextField.setText(nameText);
     }
-
     public String getDescriptionArea() {
         return descriptionArea.getText().toString();
     }
-
     public void setDescriptionArea(String description) {
         descriptionArea.setText(description);
     }
-
     public int getAgeLimitTextfield() {
-
         if(ageLimitTextfield.getText().toString()!=""){
             return Integer.valueOf(ageLimitTextfield.getText().toString());
-
         }
         return 0;
     }
-
     public void setAgeLimitTextfield(int ageLimitTextfield) {
-
         this.ageLimitTextfield.setText( String.valueOf(ageLimitTextfield));
-
     }
-
     public Double getPriceTextField() {
         return Double.valueOf(priceTextField.getText().toString());
     }
-
     public void setPriceTextField(Double priceTextField) {
         this.priceTextField.setText(String.valueOf(priceTextField));
     }
@@ -61,7 +47,16 @@ public class ActivityInfoWindow extends Application{
         noname.setContentText("Error Message");
         noname.showAndWait();
     }
-
+    public boolean validateType()
+    {
+        try{
+            Integer.valueOf(ageLimitTextfield.getText().toString()) ;
+            Double.valueOf(priceTextField.getText().toString());
+        }catch (NumberFormatException nfe){
+            return false;
+        }
+        return true;
+    }
     public boolean validate()
     {
         boolean result=true;
@@ -80,71 +75,59 @@ public class ActivityInfoWindow extends Application{
         }
         return result;
     }
-
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(){
+        window = new Stage();
+        window.setTitle("Add Adventure");
+        nameTextField = new TextField();
+        descriptionArea = new TextArea();
+        ageLimitTextfield = new TextField();
+        priceTextField = new TextField();
+        HBox nameHBox = new HBox();
+        HBox descriptionHBox = new HBox();
+        HBox priceHBox= new HBox();
+        HBox ageLimitHBox= new HBox();
         HBox deleteSaveHBox= new HBox();
-        HBox hBox = new HBox();
-        //vBox.getChildren().addAll(nameHBox,descriptionHBox,priceHBox,ageLimitHBox,deleteSaveHBox);
-        //Scene scene = new Scene(vBox, 300,300);
-
-        VBox labelVBox = new VBox();
-        VBox textVBox = new VBox();
-
-
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(nameHBox,descriptionHBox,priceHBox,ageLimitHBox,deleteSaveHBox);
+        Scene scene = new Scene(vBox, 300,300);
         Label nameLabel = new Label("Activity name: ");
+        nameHBox.getChildren().addAll(nameTextField,nameLabel );
         Label descriptionLabel = new Label("Description: ");
         descriptionArea.setPrefSize(167,100);
-
+        descriptionHBox.getChildren().addAll(descriptionArea,descriptionLabel);
         Label priceLabel = new Label("Price: ");
+        priceHBox.getChildren().addAll(priceTextField,priceLabel );
         Label ageLimitLabel = new Label("Age Limit: ");
-
-        Button deleteButton = new Button("Delete");
+        ageLimitHBox.getChildren().addAll( ageLimitTextfield,ageLimitLabel);
+        Button deleteButton = new Button("Clear");
         Button saveButton = new Button("Save");
         deleteSaveHBox.getChildren().addAll(saveButton,deleteButton);
-
-        labelVBox.getChildren().addAll(nameLabel, descriptionLabel, ageLimitLabel, priceLabel);
-        textVBox.getChildren().addAll(nameTextField, descriptionArea, ageLimitTextfield, priceTextField, deleteSaveHBox);
-        descriptionLabel.setPrefHeight(45);
-        ageLimitLabel.setTranslateY(70);
-        priceLabel.setTranslateY(75);
-        //saveButton.setTranslateX(-20);
-        deleteSaveHBox.setTranslateY(10);
-        saveButton.setTranslateX(0);
-        deleteButton.setTranslateX(0);
-        hBox.getChildren().addAll(labelVBox, textVBox);
-        Scene scene = new Scene(hBox, 300,300);
-
         deleteButton.setOnAction(event ->{
                     nameTextField.clear();
                     descriptionArea.clear();
                     priceTextField.clear();
                     ageLimitTextfield.clear();
                 }
-
         );
         saveButton.setOnAction(event -> {
-            System.out.println(validate());
-            if (validate()) {
-                boolean nameb = true;
-                boolean descb = true;
-                boolean ageb = true;
-                boolean priceb = true;
-
-
+            if (validate() && validateType()) {
+                m = new MainAdminScene();
+                String name= getNameTextField();
+                String description= getDescriptionArea();
+                int age= getAgeLimitTextfield();
+                double price= getPriceTextField();
+                ModelClass modelClass= new ModelClass();
+                modelClass.writeToDB(name,age,price,description );
                 System.out.println("Hi............");
             }
-
+            else if (validate()){
+                System.out.println("wrong type");
+            }
+            else {
+                System.out.println("fill the fields");
+            }
         });
-
-
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-
-
+        window.setScene(scene);
+        window.show();
     }
-
 }
--- src/com/company/MainAdminScene.java
+++ src/com/company/MainAdminScene.java
@@ -1,5 +1,4 @@
 package com.company;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -15,45 +14,87 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.util.ArrayList;
-
 /**
  * Created by krist on 25/02/2016.
  */
-
 public class MainAdminScene {
-
     ArrayList<Activity> activities = new ArrayList<>();
     ActivityInfoWindow a;
-
-
     TextField nameTextField = new TextField();
-
     TextArea descriptionArea = new TextArea();
     TextField ageLimitTextfield = new TextField();
     TextField priceTextField = new TextField();
-
-
+    VBox vBox;
     Stage window = new Stage();
     TableView<Activity> activityTableView;
+    public String getNameTextField() {
+        return nameTextField.getText().toString();
+    }
+    public void setNameTextField(String nameText) {
+        nameTextField.setText(nameText);
+    }
+    public String getDescriptionArea() {
+        return descriptionArea.getText().toString();
+    }
+    public void setDescriptionArea(String description) {
+        descriptionArea.setText(description);
+    }
+    public int getAgeLimitTextfield() {
+        if(ageLimitTextfield.getText().toString()!=""){
+            return Integer.valueOf(ageLimitTextfield.getText().toString());
+        }
+        return 0;
+    }
+    public void setAgeLimitTextfield(int ageLimitTextfield) {
+        this.ageLimitTextfield.setText( String.valueOf(ageLimitTextfield));
+    }
+    public Double getPriceTextField() {
+        //check if it's a double first?
+        return Double.valueOf(priceTextField.getText().toString());
+    }
+    public void setPriceTextField(Double priceTextField) {
+        this.priceTextField.setText(String.valueOf(priceTextField));
+    }
+    public void alertMessage(){
+        Alert noname = new Alert(Alert.AlertType.ERROR);
+        noname.setContentText("Error Message");
+        noname.showAndWait();
+    }
+    public boolean validate()
+    {
+        boolean result=true;
+        if(getNameTextField().equals("")) {
+            result = false;
+        }
+        if(getDescriptionArea().equals("")) {
+            result = false;
+        }
+        if(ageLimitTextfield.getText().toString().equals(""))
+        {
+            result = false;
+        }
+        if(priceTextField.getText().toString().equals("")) {
+            result = false;
+        }
+        return result;
+    }
     public void display(Stage primaryStage){
+        vBox = new VBox();
         window.setTitle("Adventure something");
         TableColumn<Activity, String> activityColumn2 = new TableColumn<>("ID");
         activityColumn2.setMinWidth(50);
         activityColumn2.setCellValueFactory(new PropertyValueFactory<>("idActivity"));
-
         TableColumn<Activity, String> activityColumn = new TableColumn<>("Activity");
         activityColumn.setMinWidth(240);
         activityColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-
         activityTableView = new TableView<>();
         activityTableView.setItems(getActivity());
         activityTableView.getColumns().add(activityColumn2);
         activityTableView.getColumns().add(activityColumn);
         activityTableView.setRowFactory(activityTableView-> {
             TableRow<Activity>row = new TableRow<Activity>();
-            row.setOnMouseClicked(event -> {if (event.getClickCount() ==2&&(! row.isEmpty())){
+            row.setOnMouseClicked(event -> {if (event.getClickCount() ==1&&(! row.isEmpty())){
                 Activity rowData = row.getItem();
                 System.out.println(rowData);
                 nameTextField.setText(rowData.getName());
@@ -64,9 +105,13 @@ public class MainAdminScene {
             });
             return row;
         });
-
         Button homeButton = new Button("Home");
         Button newButton = new Button("New");
+        newButton.setOnAction(event -> {
+            a = new ActivityInfoWindow();
+            a.start();
+            vBox.setVisible(false);
+        });
         BorderPane layout = new BorderPane();
         layout.setPadding(new Insets(10, 10, 10, 10));
         javafx.scene.image.Image image = new javafx.scene.image.Image("com/Logo2.jpg");
@@ -89,61 +134,42 @@ public class MainAdminScene {
         hBox2.getChildren().addAll(activityTableView);
         layout.setBottom(gridPane);
         layout.setCenter(hBox2);
-
-
-
         HBox nameHBox = new HBox();
         HBox descriptionHBox = new HBox();
         HBox priceHBox= new HBox();
         HBox ageLimitHBox= new HBox();
         HBox deleteSaveHBox= new HBox();
-        VBox vBox = new VBox();
         vBox.getChildren().addAll(nameHBox,descriptionHBox,priceHBox,ageLimitHBox,deleteSaveHBox);
         //Scene scene = new Scene(vBox, 300,300);
-
         Label nameLabel = new Label("Activity name: ");
-        nameHBox.getChildren().addAll(nameTextField,nameLabel );
-
+        nameHBox.getChildren().addAll(nameTextField, nameLabel );
         Label descriptionLabel = new Label("Description: ");
-
-
         descriptionArea.setPrefSize(187,100);
         descriptionHBox.getChildren().addAll(descriptionArea,descriptionLabel);
-
         Label priceLabel = new Label("Price: ");
         priceHBox.getChildren().addAll(priceTextField,priceLabel );
-
         Label ageLimitLabel = new Label("Age Limit: ");
         ageLimitHBox.getChildren().addAll( ageLimitTextfield,ageLimitLabel);
-
-        Button deleteButton = new Button("Delete");
+        Button deleteButton = new Button("Clear");
         Button saveButton = new Button("Save");
         deleteSaveHBox.getChildren().addAll(saveButton,deleteButton);
-
         deleteButton.setOnAction(event ->{
                     nameTextField.clear();
                     descriptionArea.clear();
                     priceTextField.clear();
                     ageLimitTextfield.clear();
-
                 }
-
         );
-
-
         System.out.println(activities.get(0).getName());
         saveButton.setOnAction(event -> {
             System.out.println(validate());
             if (validate()) {
-
-
                 boolean nameb = true;
                 boolean descb = true;
                 boolean ageb = true;
                 boolean priceb = true;
                 String name = getNameTextField();
                 if (name.equals("")) {
-
                     nameb = false;
                 }
                 String description = getDescriptionArea();
@@ -155,37 +181,23 @@ public class MainAdminScene {
                 if (age == null) {
                     ageb = false;
                 }
-
                 Double price = getPriceTextField();
                 if (price == null) {
                     priceb = false;
                 }
-
                 if (nameb == false || descb == false || ageb == false || priceb == false) {
                     alertMessage();
                 }
-
-
                 System.out.println("Hi............");
             }
-
-
-
         });
-
         layout.setRight(vBox);
-
-
         Scene scene = new Scene(layout, 600, 500);
         window.setScene(scene);
-
-
         homeButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
-
                 View view = new View();
-
                 Stage window2 = new Stage();
                 view.start(primaryStage);
             }
@@ -196,92 +208,13 @@ public class MainAdminScene {
         ModelClass modelClass = new ModelClass();
         ArrayList<Activity> list = new ArrayList<>();
         ObservableList<Activity> activities2 = FXCollections.observableArrayList();
-
         list = modelClass.getDBactivities();
-
         for (Activity a: list)
         {
             activities.add(a);
             activities2.add(a);
             System.out.println(a.toString());
         }
-
         return activities2;
-    }
-
-
-
-
-
-
-
-    public String getNameTextField() {
-        return nameTextField.getText().toString();
-    }
-
-    public void setNameTextField(String nameText) {
-        nameTextField.setText(nameText);
-    }
-
-    public String getDescriptionArea() {
-        return descriptionArea.getText().toString();
-    }
-
-    public void setDescriptionArea(String description) {
-        descriptionArea.setText(description);
-    }
-
-    public int getAgeLimitTextfield() {
-
-        if(ageLimitTextfield.getText().toString()!=""){
-            return Integer.valueOf(ageLimitTextfield.getText().toString());
-
-        }
-        return 0;
-    }
-
-    public void setAgeLimitTextfield(int ageLimitTextfield) {
-
-        this.ageLimitTextfield.setText( String.valueOf(ageLimitTextfield));
-
-    }
-
-    public Double getPriceTextField() {
-        //check if it's a double first?
-        return Double.valueOf(priceTextField.getText().toString());
-    }
-
-    public void setPriceTextField(Double priceTextField) {
-        this.priceTextField.setText(String.valueOf(priceTextField));
-    }
-    public void alertMessage(){
-        Alert noname = new Alert(Alert.AlertType.ERROR);
-        noname.setContentText("Error Message");
-        noname.showAndWait();
-    }
-    public boolean validate()
-    {
-        boolean result=true;
-        if(getNameTextField().equals("")) {
-            result = false;
-        }
-
-
-        if(getDescriptionArea().equals("")) {
-            result = false;
-        }
-
-
-        if(ageLimitTextfield.getText().toString().equals(""))
-        {
-            result = false;
-        }
-
-
-        if(priceTextField.getText().toString().equals("")) {
-            result = false;
-        }
-
-        return result;
     }
 }
