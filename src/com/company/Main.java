8be3c55ce7d9f85f037151f1caf371c9ce25f457
Everest#1 <kaspars.cinis@gmail.com>
2016-02-26 12:12:29 +0100

    The code works now

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 
 public class Main extends Application {
 
-    /*  @Test //#1
+      @Test //#1
     public void testDBinsertion()
     {
     ModelClass modelClass = new ModelClass();
@@ -30,14 +30,12 @@ public class Main extends Application {
     public void testDBupdate()
     {
         ModelClass modelClass = new ModelClass();
-<<<<<<< HEAD
         ArrayList<Activity> list = new ArrayList<>();
         list = modelClass.getDBactivities();
 
-=======
+
         modelClass.writeToDB("DD", 12, 25, "sdsd");
         assertEquals("DD", modelClass.toString());
->>>>>>> origin/master
 
         modelClass.updateDB("testActivity", 15, 20, "This is an description", list.get(list.size()-1).getIdActivity());
     }
@@ -46,34 +44,25 @@ public class Main extends Application {
     public void testDBdelete()
     {
         ModelClass modelClass = new ModelClass();
-<<<<<<< HEAD
         assertNotNull(modelClass.getDBactivities2("4").getName());
         //System.out.println(modelClass.getDBactivities2("2").toString());
 
 
-=======
         modelClass.writeToDB("DD", 12, 25, "sdsd");
         assertEquals("DD", modelClass.toString());
->>>>>>> origin/master
     }
 
     @Test //#5
     public void testDBretrieve()
     {
         ModelClass modelClass = new ModelClass();
-<<<<<<< HEAD
         ArrayList<Activity> list = new ArrayList<>();
         list = modelClass.getDBactivities();
 
 
         modelClass.deleteDB(list.get(list.size() - 1).getIdActivity());
     }
-=======
-        modelClass.writeToDB("DD", 12, 25, "sdsd");
-        assertEquals("DD", modelClass.toString());
-    } */
-
->>>>>>> origin/master
+    
 
     MainAdminScene m = new MainAdminScene();
     Stage window;
