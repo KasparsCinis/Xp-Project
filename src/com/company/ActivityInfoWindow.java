1d7c361f033bfbfb208e69d53d4bfa26c6589c32
Dpavlova <doni0070@stud.kea.dk>
2016-02-26 13:03:08 +0100

    Delete repeated and unused code in ActivityInfoWindow

--- src/com/company/ActivityInfoWindow.java
+++ src/com/company/ActivityInfoWindow.java
@@ -13,7 +13,6 @@ import javafx.stage.Stage;
 public class ActivityInfoWindow extends Application{
 
     TextField nameTextField = new TextField();
-
     TextArea descriptionArea = new TextArea();
     TextField ageLimitTextfield = new TextField();
     TextField priceTextField = new TextField();
@@ -62,41 +61,30 @@ public class ActivityInfoWindow extends Application{
         noname.setContentText("Error Message");
         noname.showAndWait();
     }
+
     public boolean validate()
     {
         boolean result=true;
         if(getNameTextField().equals("")) {
             result = false;
         }
-
-
         if(getDescriptionArea().equals("")) {
             result = false;
         }
-
-
         if(ageLimitTextfield.getText().toString().equals(""))
         {
             result = false;
         }
-
-
         if(priceTextField.getText().toString().equals("")) {
             result = false;
         }
-
         return result;
     }
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        HBox nameHBox = new HBox();
-        HBox descriptionHBox = new HBox();
-        HBox priceHBox= new HBox();
-        HBox ageLimitHBox= new HBox();
         HBox deleteSaveHBox= new HBox();
-        //VBox vBox = new VBox();
         HBox hBox = new HBox();
         //vBox.getChildren().addAll(nameHBox,descriptionHBox,priceHBox,ageLimitHBox,deleteSaveHBox);
         //Scene scene = new Scene(vBox, 300,300);
@@ -106,19 +94,11 @@ public class ActivityInfoWindow extends Application{
 
 
         Label nameLabel = new Label("Activity name: ");
-        nameHBox.getChildren().addAll(nameTextField,nameLabel );
-
         Label descriptionLabel = new Label("Description: ");
-
-
         descriptionArea.setPrefSize(167,100);
-        descriptionHBox.getChildren().addAll(descriptionArea,descriptionLabel);
 
         Label priceLabel = new Label("Price: ");
-        priceHBox.getChildren().addAll(priceTextField,priceLabel );
-
         Label ageLimitLabel = new Label("Age Limit: ");
-        ageLimitHBox.getChildren().addAll( ageLimitTextfield,ageLimitLabel);
 
         Button deleteButton = new Button("Delete");
         Button saveButton = new Button("Save");
@@ -141,53 +121,22 @@ public class ActivityInfoWindow extends Application{
                     descriptionArea.clear();
                     priceTextField.clear();
                     ageLimitTextfield.clear();
-
                 }
 
         );
         saveButton.setOnAction(event -> {
             System.out.println(validate());
             if (validate()) {
-
-
                 boolean nameb = true;
                 boolean descb = true;
                 boolean ageb = true;
                 boolean priceb = true;
-                String name = getNameTextField();
-                if (name.equals("")) {
-
-                    nameb = false;
-                }
-                String description = getDescriptionArea();
-                if (description.equals("")) {
-                    descb = false;
-                }
-                getAgeLimitTextfield();
-                Integer age = getAgeLimitTextfield();
-                if (age == null) {
-                    ageb = false;
-                }
-
-                Double price = getPriceTextField();
-                if (price == null) {
-                    priceb = false;
-                }
-
-                if (nameb == false || descb == false || ageb == false || priceb == false) {
-                    alertMessage();
-                }
 
 
                 System.out.println("Hi............");
             }
 
-
-
         });
-
-
-
 
 
 
