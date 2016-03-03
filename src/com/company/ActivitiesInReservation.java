8412a1be8d82df960f2398874273679e75e99a4a
Everest#1 <kaspars.cinis@gmail.com>
2016-03-03 13:34:58 +0100

    Updated some database stuff, everything is broken now

---
+++ src/com/company/ActivitiesInReservation.java
@@ -0,0 +1,63 @@
+package com.company;
+
+/**
+ * Created by kasdi on 03.03.2016.
+ */
+public class ActivitiesInReservation {
+
+    private int activitiesInReservationID;
+    private int idReservation;
+    private int idActivity;
+    private int startTime;
+    private int endTime;
+
+    public int getActivitiesInReservationID() {
+        return activitiesInReservationID;
+    }
+
+    public void setActivitiesInReservationID(int activitiesInReservationID) {
+        this.activitiesInReservationID = activitiesInReservationID;
+    }
+
+    public int getIdReservation() {
+        return idReservation;
+    }
+
+    public void setIdReservation(int idReservation) {
+        this.idReservation = idReservation;
+    }
+
+    public int getIdActivity() {
+        return idActivity;
+    }
+
+    public void setIdActivity(int idActivity) {
+        this.idActivity = idActivity;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+
+    public ActivitiesInReservation(int activitiesInReservationID, int idReservation, int idActivity, int startTime, int endTime) {
+        this.activitiesInReservationID = activitiesInReservationID;
+        this.idReservation = idReservation;
+        this.idActivity = idActivity;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+
+}
